868ed468dc743a8df05e877fa7f355bee4f84845
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-07-18 12:31:35 +0200

    test switch data

--- pages/login.vue
+++ pages/login.vue
@@ -2,12 +2,32 @@
     <main>
         <h1>Page login</h1>
         <button><nuxt-link :to="{name: 'home'}">home</nuxt-link></button>
+        <input type="text" name="" id="" v-model="docState">
+        <transition>
+          <button v-bind:key="docState">
+            {{ buttonMessage }}
+          </button>
+        </transition>
 
     </main>
 </template>
 
 <script>
 export default {
-  layout: 'landing'
+  layout: 'landing',
+  data () {
+    return {
+      docState: ""
+    }
+  },
+  computed: {
+  buttonMessage: function () {
+    switch (this.docState) {
+      case 'saved': return 'Modifier'
+      case 'edited': return 'Sauver'
+      case 'editing': return 'Annuler'
+    }
+  }
+}
 }
 </script>
