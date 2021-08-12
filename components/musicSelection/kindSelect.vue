1c1c99968884bd59ccea1aac6787bddf9f366b37
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-08-12 15:23:13 +0200

    display kind select

--- components/musicSelection/kindSelect.vue
+++ components/musicSelection/kindSelect.vue
@@ -1,13 +1,21 @@
 <template>
-    <fieldset>
+    <fieldset class="row m-3 border">
         <legend>{{title}} </legend>
 
-        <input type="checkbox" value="classique" name="musicType" v-model="musicType">
-        <label for="kraken">classique</label><br/>
-        <input type="checkbox" value="rocks" name="musicType" v-model="musicType">
-        <label for="sasquatch">pop rocks</label><br/>
-        <input type="checkbox" value="dance" name="musicType" v-model="musicType">
-        <label for="mothman">dance</label>
+        <div class="col-12">
+            <input type="checkbox" value="pop rocks" name="musicType" v-model="musicType">
+            <label for="pop rocks">pop rocks</label><br/>
+        </div>
+        
+        <div class="col-12">
+            <input type="checkbox" value="dance" name="musicType" v-model="musicType">
+            <label for="dance">dance</label>
+        </div>
+
+        <div class="col-12">
+            <input type="checkbox" value="latino" name="musicType" v-model="musicType">
+            <label for="latino">latino</label>
+        </div>
 
         <input type="button" value="next" @click="next">     
 
