d4c54cf3be9a1906909f24ff9bfce5d389575830
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-08-12 17:24:12 +0200

    test toogle nav index

--- components/navBar.vue
+++ components/navBar.vue
@@ -1,12 +1,21 @@
 <template>
   <header class="row align-items-center border p-5">
 
-    <p class="col-6">LOGO</p>
+    <p class="col-4">LOGO</p>
+
     <nav class="col-6 text-right">
       <nuxt-link :to="{name: 'index'}" class="p-4">Accueil</nuxt-link>
       <nuxt-link :to="{name: 'login'}" class="p-4">Se connecter</nuxt-link>
       <nuxt-link :to="{name: 'signup'}" class="p-4">S'inscrire</nuxt-link>
-
     </nav>
+
+    <nav class="col-2">
+    <b-dropdown class="mx-1" left text="menu">
+      <b-link class="d-block p-3" href="#concept">concept</b-link>
+      <b-link class="d-block p-3" href="#vision">vision</b-link>
+      <b-link class="d-block p-3" href="#offre">offre</b-link>
+    </b-dropdown> 
+    </nav>
+
   </header>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,35 @@
 <template>
   <main class="row text-center">
 
-    <aside>
-    <b-dropdown class="mx-1" right text="menu">
-      <b-link href="#concept">concept</b-link>
-      <b-link href="#vision">vision</b-link>
-      <b-link href="#offre">offre</b-link>
-    </b-dropdown>    
+    <div data-spy="scroll" data-target="#navbar-example2" data-offset="0">
 
-    </aside>
+      <article class="col-12 m-3">
+        <h1 id="concept">Présentation du concept</h1>
+        <p>La définition d'une valeur la suggère comme nom de fichier. Les caractères / et \ sont convertis en caractères de soulignement (_). Les systèmes de fichiers peuvent interdire d'autres caractères dans les noms de fichiers, les navigateurs ajusteront donc le nom suggéré si nécessaire.</p>
+        <p>La définition d'une valeur la suggère comme nom de fichier. Les caractères / et \ sont convertis en caractères de soulignement (_). Les systèmes de fichiers peuvent interdire d'autres caractères dans les noms de fichiers, les navigateurs ajusteront donc le nom suggéré si nécessaire.</p>
+        <p>La définition d'une valeur la suggère comme nom de fichier. Les caractères / et \ sont convertis en caractères de soulignement (_). Les systèmes de fichiers peuvent interdire d'autres caractères dans les noms de fichiers, les navigateurs ajusteront donc le nom suggéré si nécessaire.</p>
+      </article>
 
-    <article id="concept" class="col-12 m-3">
-      <h1>Présentation du concept</h1>
-      <p>La définition d'une valeur la suggère comme nom de fichier. Les caractères / et \ sont convertis en caractères de soulignement (_). Les systèmes de fichiers peuvent interdire d'autres caractères dans les noms de fichiers, les navigateurs ajusteront donc le nom suggéré si nécessaire.</p>
-      <p>La définition d'une valeur la suggère comme nom de fichier. Les caractères / et \ sont convertis en caractères de soulignement (_). Les systèmes de fichiers peuvent interdire d'autres caractères dans les noms de fichiers, les navigateurs ajusteront donc le nom suggéré si nécessaire.</p>
-      <p>La définition d'une valeur la suggère comme nom de fichier. Les caractères / et \ sont convertis en caractères de soulignement (_). Les systèmes de fichiers peuvent interdire d'autres caractères dans les noms de fichiers, les navigateurs ajusteront donc le nom suggéré si nécessaire.</p>
-    </article>
+      <article class="col-12 m-3">
+        <h1 id="vision">Notre vision</h1>
+        <p>La définition d'une valeur la suggère comme nom de fichier. Les caractères / et \ sont convertis en caractères de soulignement (_). Les systèmes de fichiers peuvent interdire d'autres caractères dans les noms de fichiers, les navigateurs ajusteront donc le nom suggéré si nécessaire.</p>
+        <p>La définition d'une valeur la suggère comme nom de fichier. Les caractères / et \ sont convertis en caractères de soulignement (_). Les systèmes de fichiers peuvent interdire d'autres caractères dans les noms de fichiers, les navigateurs ajusteront donc le nom suggéré si nécessaire.</p>
+        <p>La définition d'une valeur la suggère comme nom de fichier. Les caractères / et \ sont convertis en caractères de soulignement (_). Les systèmes de fichiers peuvent interdire d'autres caractères dans les noms de fichiers, les navigateurs ajusteront donc le nom suggéré si nécessaire.</p>
+      </article>
 
-    <article id="vision" class="col-12 m-3">
-      <h1>Notre vision</h1>
-      <p>La définition d'une valeur la suggère comme nom de fichier. Les caractères / et \ sont convertis en caractères de soulignement (_). Les systèmes de fichiers peuvent interdire d'autres caractères dans les noms de fichiers, les navigateurs ajusteront donc le nom suggéré si nécessaire.</p>
-      <p>La définition d'une valeur la suggère comme nom de fichier. Les caractères / et \ sont convertis en caractères de soulignement (_). Les systèmes de fichiers peuvent interdire d'autres caractères dans les noms de fichiers, les navigateurs ajusteront donc le nom suggéré si nécessaire.</p>
-      <p>La définition d'une valeur la suggère comme nom de fichier. Les caractères / et \ sont convertis en caractères de soulignement (_). Les systèmes de fichiers peuvent interdire d'autres caractères dans les noms de fichiers, les navigateurs ajusteront donc le nom suggéré si nécessaire.</p>
-    </article>
+      <article class="col-12 m-3">
+        <h1>Pourquoi</h1>
+        <p>La définition d'une valeur la suggère comme nom de fichier. Les caractères / et \ sont convertis en caractères de soulignement (_). Les systèmes de fichiers peuvent interdire d'autres caractères dans les noms de fichiers, les navigateurs ajusteront donc le nom suggéré si nécessaire.</p>
+        <p>La définition d'une valeur la suggère comme nom de fichier. Les caractères / et \ sont convertis en caractères de soulignement (_). Les systèmes de fichiers peuvent interdire d'autres caractères dans les noms de fichiers, les navigateurs ajusteront donc le nom suggéré si nécessaire.</p>
+        <p>La définition d'une valeur la suggère comme nom de fichier. Les caractères / et \ sont convertis en caractères de soulignement (_). Les systèmes de fichiers peuvent interdire d'autres caractères dans les noms de fichiers, les navigateurs ajusteront donc le nom suggéré si nécessaire.</p>
+      </article>
 
-    <article class="col-12 m-3">
-      <h1>Pourquoi</h1>
-      <p>La définition d'une valeur la suggère comme nom de fichier. Les caractères / et \ sont convertis en caractères de soulignement (_). Les systèmes de fichiers peuvent interdire d'autres caractères dans les noms de fichiers, les navigateurs ajusteront donc le nom suggéré si nécessaire.</p>
-      <p>La définition d'une valeur la suggère comme nom de fichier. Les caractères / et \ sont convertis en caractères de soulignement (_). Les systèmes de fichiers peuvent interdire d'autres caractères dans les noms de fichiers, les navigateurs ajusteront donc le nom suggéré si nécessaire.</p>
-      <p>La définition d'une valeur la suggère comme nom de fichier. Les caractères / et \ sont convertis en caractères de soulignement (_). Les systèmes de fichiers peuvent interdire d'autres caractères dans les noms de fichiers, les navigateurs ajusteront donc le nom suggéré si nécessaire.</p>
-    </article>
-
-    <article id="offre" class="col-12 m-3">
-      <h1>Comprendre notre offre</h1>
-      <p>La définition d'une valeur la suggère comme nom de fichier. Les caractères / et \ sont convertis en caractères de soulignement (_). Les systèmes de fichiers peuvent interdire d'autres caractères dans les noms de fichiers, les navigateurs ajusteront donc le nom suggéré si nécessaire.</p>
-    </article>
+      <article class="col-12 m-3">
+        <h1 id="offre">Comprendre notre offre</h1>
+        <p>La définition d'une valeur la suggère comme nom de fichier. Les caractères / et \ sont convertis en caractères de soulignement (_). Les systèmes de fichiers peuvent interdire d'autres caractères dans les noms de fichiers, les navigateurs ajusteront donc le nom suggéré si nécessaire.</p>
+      </article>
+    </div>
+    
     
   </main>
 </template>
