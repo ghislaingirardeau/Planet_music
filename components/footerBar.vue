e78ce57ecf55f2859dc58c23522399c1cf95bd11
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-08-13 15:25:50 +0200

    test add footer components

---
+++ components/footerBar.vue
@@ -0,0 +1,11 @@
+<template>
+    <footer class="container p-3">
+        <ul class="list-group list-group-horizontal">
+            <li class="list-group-item">About us</li>
+            <li class="list-group-item">Help</li>
+            <li class="list-group-item">Terms</li>
+            <li class="list-group-item">Privacy</li>
+            <li class="list-group-item">Copyright</li>
+        </ul>
+    </footer>
+</template>
